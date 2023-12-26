ef91b7f2cff82789381b2b0a3d0b80e1fe6c304c
Dario Sechi <dario.sechi.rnd@gmail.com>
2023-12-26 15:52:24 +0100

    Make the type strictness of observe more permissive

--- src/functions/observe.ts
+++ src/functions/observe.ts
@@ -1,5 +1,8 @@
+import { KeyOf } from '@aracna/core'
 import { Observable } from '../classes/observable.js'
 
-export function observe<T extends object, K extends keyof T>(target: T, keys?: K[]): T {
+export function observe<T extends object>(target: T, keys?: KeyOf.Shallow<T>[]): T
+export function observe<T extends object>(target: T, keys?: string[]): T
+export function observe<T extends object>(target: T, keys?: KeyOf.Shallow<T>[]): T {
   return Observable.make(target, keys)
 }
